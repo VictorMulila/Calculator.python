d839f8795e7f339099e6cca88dfe200f582f91ee
VictorMulila <146417375+VictorMulila@users.noreply.github.com>
2023-11-10 02:18:40 +0300

    Update Calc.py

--- Calc.py
+++ Calc.py
@@ -1,21 +1,25 @@
 #Victor mulila
 #sct211-0049/2022
-
-def add(x, y):
+class Calculator:
+ def__innit__(self,x,y):
+    self.x=x
+     self.y=y
+   
+ def add(self,x, y):
   """Returns the sum of two numbers."""
-  return x + y
+   return x + y
 
-def subtract(x, y):
+ def subtract(self,x, y):
   """Returns the difference of two numbers."""
-  return x - y
+   return x - y
 
-def multiply(x, y):
+ def multiply(self,x, y):
   """Returns the product of two numbers."""
-  return x * y
+   return x * y
 
-def divide(x, y):
+ def divide(self,x, y):
   """Returns the quotient of two numbers."""
-  return x / y
+   return x / y
 
 
 print("Please select the operation you want to perform:")
@@ -29,16 +33,16 @@ choice = input("Enter your choice (1, 2, 3, or 4): ")
 
 num1 = float(input("Enter the first number: "))
 num2 = float(input("Enter the second number: "))
-
+calc=Calculator()
 
 if choice == "1":
-  result = add(num1, num2)
+  result = calc.add(num1, num2)
 elif choice == "2":
-  result = subtract(num1, num2)
+  result = calc.subtract(num1, num2)
 elif choice == "3":
-  result = multiply(num1, num2)
+  result = calc.multiply(num1, num2)
 elif choice == "4":
-  result = divide(num1, num2)
+  result = calc.divide(num1, num2)
 else:
   print("Invalid choice.")
   exit()
